abf975a0f34d78477639d1c0a6235e10ffac8d06
Belati <belatijagadbintangsyuhada@gmail.com>
2023-10-26 22:45:49 +0700

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/templates/**/*.html',
+    './**/templatetags/**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
