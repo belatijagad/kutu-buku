b05ff1aa43f581554fd0bac38c69a6d12a8d5a96
Belati <belatijagadbintangsyuhada@gmail.com>
2023-10-29 16:31:15 +0700

    navbar ketika signin dan tidak

--- autentikasi/views.py
+++ autentikasi/views.py
@@ -1,57 +1,45 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login as auth_login
-from django.http import JsonResponse
+import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.urls import reverse
+from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-# Create your views here.
 
-@csrf_exempt
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            auth_login(request, user)
-            # Redirect to a success page.
-            return JsonResponse({
-              "status": True,
-              "message": "Log In Successful!",
-              # Insert any extra data if you want to pass data to Flutter
-              # show username and password
-              "username": username,
-              "password": password
-            }, status=200)
-        else:
-            return JsonResponse({
-              "status": False,
-              "message": "Login Failed, Account Disabled."
-            }, status=401)
-
-    else:
-        return JsonResponse({
-          "status": False,
-          "message": "Login Failed, Check Your Password."
-        }, status=401)
-
-@csrf_exempt
-def logout(request):
-    logout(request)
-    return JsonResponse({
-      "status": True,
-      "message": "Log Out Successful!"
-    }, status=200)
-
-@csrf_exempt
 def register(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = User.objects.create_user(username, password)
-    user.save()
-    return JsonResponse({
-      "status": True,
-      "message": "Registration Successful!!",
-      "username": username,
-      "password": password
-    }, status=200)
+    form = UserCreationForm()
+    submitted = False
 
+    if request.method == "POST":
+      form = UserCreationForm(request.POST)
+      submitted = True
+      if form.is_valid():
+        form.save()
+        messages.success(request, 'Your account has been successfully created!')
+        return redirect('autentikasi:login')
+    context = {'form':form, 'submitted': submitted}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('autentikasi:login'))
+    response.delete_cookie('last_login')
+    return response
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -4,6 +4,6 @@ from bookmark.views import show_main, show_json_by_id
 app_name = 'daftar_membaca'
 
 urlpatterns = [
-    path('daftar-membaca', show_main, name='show_main'),
+    path('daftar-membaca', show_main, name='daftar_membaca'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
