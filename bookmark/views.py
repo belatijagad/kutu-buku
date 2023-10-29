f456e56f2fc0a3cc3cde57e92dbf8c06b3f2a9ae
Belati <belatijagadbintangsyuhada@gmail.com>
2023-10-29 23:38:32 +0700

    hotfix

--- bookmark/views.py
+++ bookmark/views.py
@@ -9,7 +9,7 @@ def show_json_by_id(request, id):
     data = Book.objects.filter(pk=id)
     return HttpResponse (serializers.serialize("json", data), content_type="application/json")
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def show_main(request):
     context = {
         'novels': Book.objects.all(),
