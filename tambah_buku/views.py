3f5cfce8d7331e72e7446ef2b00bb9f40d7256f0
Thifaalinno <falinno.abdi@gmail.com>
2023-10-29 10:47:58 +0700

    branch tambah_buku

--- tambah_buku/views.py
+++ tambah_buku/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
 
-# Create your views here.
+def add_book(request):
+    if not request.user.is_authenticated:
+        return redirect('login')  # Redirect ke halaman login jika pengguna belum masuk.
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Ganti 'book_list' dengan nama URL daftar buku.
+
+    else:
+        form = BookForm()
+
+    return render(request, 'add_book.html', {'form': form})
